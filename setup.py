327307e4649c4b2bc9c4387e3726fc5732385765
Yunfei Wang <Yunfei.Wang19@imperial.ac.uk>
2022-05-15 01:52:49 +0100

    Exercise

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="polynomials",
+    version="0.1",
+    packages=find_packages()
+)
+
+setup(
+    name="shape",
+    version="0.1",
+    packages=find_packages()
+)
